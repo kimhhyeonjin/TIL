4df5382a9b786bc048a9d198c75fe08bd1f10837
kimhhyeonjin <kimhhyeonjin@gmail.com>
2022-09-14 22:58:32 +0900

    0914 commit

---
+++ Django/django_1/articles/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Article
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+
+def create(request):
+    title = request.GET.get('title')
+    return render(request, 'articles/create.html', context)
