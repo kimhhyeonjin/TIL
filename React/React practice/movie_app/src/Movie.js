8f051e7423b7687aae16a65002e68613b3310266
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-01-10 15:48:08 +0900

    0110 commit

--- React/React practice/movie_app/src/Movie.js
+++ React/React practice/movie_app/src/Movie.js
@@ -4,15 +4,26 @@ import PropTypes from "prop-types";
 // state가 필요하지 않은 경우는 굳이 class로 할 필요없이
 // function으로 만들어도 됨
 function Movie({id, year, title, summary, poster}){
-    return <h4>{title}</h4>
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title" style={{ backgroundColor : "red" }}>
+          {title}
+        </h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
-    year: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
 }
 
 export default Movie;
