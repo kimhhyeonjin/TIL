225897a2123b411d0459f71b7cbc9c110019cfe0
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-01-10 16:33:25 +0900

    0110 commit

--- React/React practice/movie_app/src/Movie.js
+++ React/React practice/movie_app/src/Movie.js
@@ -3,16 +3,24 @@ import PropTypes from "prop-types";
 
 // state가 필요하지 않은 경우는 굳이 class로 할 필요없이
 // function으로 만들어도 됨
-function Movie({id, year, title, summary, poster}){
+function Movie({id, year, title, summary, poster, genres}){
   return (
-    <div class="movie">
+    <div className="movie">
+      <label htmlFor=""></label>
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data">
-        <h3 class="movie__title" style={{ backgroundColor : "red" }}>
+      <div className="movie__data">
+        <h3 className="movie__title">
           {title}
         </h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
@@ -23,7 +31,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
-}
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
+};
 
 export default Movie;
