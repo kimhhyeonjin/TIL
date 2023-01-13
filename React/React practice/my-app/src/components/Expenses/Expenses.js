8184d0203157e9cba4061ef49f0ca1fd00b1dc54
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-01-13 13:56:40 +0900

    0113 commit

--- React/React practice/my-app/src/components/Expenses/Expenses.js
+++ React/React practice/my-app/src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
@@ -15,26 +15,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpenseList items={filteredExpenses} />
     </Card>
   );
 };
--- React/React practice/my-app/src/components/NewExpense/ExpenseForm.js
+++ React/React practice/my-app/src/components/NewExpense/ExpenseForm.js
@@ -2,66 +2,20 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-  // 첫 번째 방법
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
-  // 두 번째, 세 번째 방법
-  //   const [userInput, setUserInput] = useState({
-  //     enteredTitle: "",
-  //     enteredAmount: "",
-  //     enteredDate: "",
-  //   });
-
-  // EventListener
   const titleChangeHandler = (event) => {
-    // 첫 번째 방법
     setEnteredTitle(event.target.value);
-
-    // 두 번째 방법
-    // setUserInput({
-    // 기존의 값을 불러온 후 수정할 부분을 업데이트
-    //   ...userInput,
-    //   enteredTitle: event.target.value,
-    // });
-
-    // 세 번째 방법(권장)
-    // setUserInput((prevState) => {
-    //     return { ...prevState, enteredTitle: event.target.value };
-    // });
   };
 
   const amountChangeHandler = (event) => {
-    // 첫 번째 방법
     setEnteredAmount(event.target.value);
-
-    // 두 번째 방법
-    // setUserInput({
-    //   ...userInput,
-    //   enteredAmount: event.target.value,
-    // });
-
-    // 세 번째 방법(권장)
-    // setUserInput((prevState) => {
-    //     return { ...prevState, enteredAmout: event.target.value };
-    // });
   };
 
   const dateChangeHandler = (event) => {
-    // 첫 번째 방법
     setEnteredDate(event.target.value);
-
-    // 두 번째 방법
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: event.target.value,
-    // });
-
-    // 세 번째 방법(권장)
-    // setUserInput((prevState) => {
-    //     return { ...prevState, enteredDate: event.target.value };
-    // });
   };
 
   const submitHandler = (event) => {
@@ -111,6 +65,7 @@ const ExpenseForm = (props) => {
         />
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
