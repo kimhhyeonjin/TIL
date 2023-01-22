4e1b0de84f41ebb471193f5784b50e84624db243
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-01-23 03:35:07 +0900

    0123 commit

---
+++ React/React practice/section16-app/src/components/SimpleInput.js
@@ -0,0 +1,43 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value; 
+    console.log(enteredValue);
+
+    // nameInputRef를 이용하는 방법은
+    // DOM을 직접 조작하는 것이므로 바람직하지 않음
+    nameInputRef.current.value = '';
+    setEnteredName('');
+  };
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
