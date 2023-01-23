b6e276bbba2c0b25d8525dfed3918eec45fb66c0
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-01-24 02:12:24 +0900

    0124 commit

--- React/React practice/section16-app/src/components/SimpleInput.js
+++ React/React practice/section16-app/src/components/SimpleInput.js
@@ -3,6 +3,8 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -11,19 +13,34 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
     console.log(enteredName);
-    const enteredValue = nameInputRef.current.value; 
+    const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
 
     // nameInputRef를 이용하는 방법은
     // DOM을 직접 조작하는 것이므로 바람직하지 않음
-    nameInputRef.current.value = '';
-    setEnteredName('');
+    nameInputRef.current.value = "";
+    setEnteredName("");
   };
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -32,6 +49,9 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
