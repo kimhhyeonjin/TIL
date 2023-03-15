f5f920dbe65ec02369087f99e4ffc42266f5764e
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-03-15 16:10:50 +0900

    0315 commit

--- React/React practice/section19-app/src/App.js
+++ React/React practice/section19-app/src/App.js
@@ -1,17 +1,83 @@
-import { useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  // cart가 변경되면 useEffect를 통해 컴포넌트가 재실행되면서
+  // 최신 버전의 cart가 받아와짐
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://create-http-77654-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+
+    // 제일 처음 렌더링 됐을 때는 sendCartData가 실행되지 않도록 설정
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- React/React practice/section19-app/src/components/Shop/ProductItem.js
+++ React/React practice/section19-app/src/components/Shop/ProductItem.js
@@ -1,36 +1,64 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+// 리덕스로 작업할 때 비동기 코드를 넣는 방법 1
+// import { useSelector } from "react-redux";
 
 import { cartActions } from "../../store/cart-slice";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
-  const cart = useSelector((state) => state.cart);
+  // 리덕스로 작업할 때 비동기 코드를 넣는 방법 1
+  // const cart = useSelector((state) => state.cart);
+
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    const newTotalQuantity = cart.totalQuantity + 1;
+    // 리덕스로 작업할 때 비동기 코드를 넣는 방법 1
+    // 자체 action creator 함수를 생성하여 리덕스 툴킷을 이용하지 않고 작성
+    // // 리덕스 툴킷이 아니므로 cart.totalQuantity 자체를 수정해서는 안됨
+    // const newTotalQuantity = cart.totalQuantity + 1;
 
-    const updatedItems = cart.items.slice();
-    const existingItem = updatedItems.find((item) => item.id === id);
-    if (existingItem) {
-      const updatedItem = { ...existingItem };
-      updatedItem.quantity++;
-      updatedItem.price = updatedItem.price + price;
-      const existingItemIndex = updatedItems.findIndex(
-        (item) => item.id === id
-      );
-    }
+    // // cart.items 자체를 수정할 수 없으므로 updatedItems로 복사
+    // const updatedItems = cart.items.slice();
+    // // 복사한 항목 중 id가 일치하는 것만을 existingItem에 저장
+    // const existingItem = updatedItems.find((item) => item.id === id);
+    // if (existingItem) {
+    //   // existingItem 또한 리덕스 스토어의 일부인 메모리 객체이므로
+    //   // updatedItem이라는 새 객체에 복사하여 사용
+    //   const updatedItem = { ...existingItem };
+    //   updatedItem.quantity++;
+    //   updatedItem.price = updatedItem.price + price;
+    //   const existingItemIndex = updatedItems.findIndex(
+    //     (item) => item.id === id
+    //   );
+    //   updatedItems[existingItemIndex] = updatedItem;
+    // } else {
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     name: title,
+    //   });
+    // }
 
-    // dispatch(
-    //   cartActions.addItemToCart({
-    //     id,
-    //     title,
-    //     price,
-    //   })
-    // );
+    // const newCart = {
+    //   totalQuantity: newTotalQuantity,
+    //   items: updatedItems,
+    // };
+
+    // dispatch(cartActions.replaceCart(newCart));
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
   };
 
   return (
