59fdfb13ff7cedbb13b71d1a77d88c64847900ea
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-04-20 17:52:29 +0900

    0420 commit

---
+++ NextJs/nextjs-course-v10-2/pages/[meetupId]/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+const MeetupDetails = () => {
+  return (
+    <Fragment>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Funguni_Island%2C_Pangani%2C_Tanga.jpg/1280px-Funguni_Island%2C_Pangani%2C_Tanga.jpg"
+        alt="A First Meetup"
+      />
+      <h1>A First Meetup</h1>
+      <address>Some Street 5, Some City</address>
+      <p>The meetup description</p>
+    </Fragment>
+  );
+};
+
+export default MeetupDetails;
