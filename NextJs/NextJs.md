c515240410c80253406fc876a99f8a5ae42cd0bc
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-04-25 17:18:01 +0900

    0425 commit

--- NextJs/NextJs.md
+++ NextJs/NextJs.md
@@ -615,3 +615,50 @@
       
       export default NewMeetupPage;
       ```
+    
+    - 데이터베이스에서 데이터 가져오기
+      
+      ```js
+      import { MongoClient } from "mongodb";
+      import MeetupList from "../components/meetups/MeetupList";
+      
+      const HomePage = (props) => {
+        return <MeetupList meetups={props.meetups} />;
+      };
+      
+      export async function getStaticProps() {
+        // MongoClient 연결
+        const client = await MongoClient.connect(
+          "mongodb+srv://<username>:<password>@cluster0.uqu8pq0.mongodb.net/meetups?retryWrites=true&w=majority"
+        );
+      
+        // meetups에 연결 중인 데이터베이스 받아오기
+        // meetups 데이터베이스가 없는 경우 바로 생성됨
+        const db = client.db();
+      
+        // meetups라는 이름의 collection에 접근
+        // meetups collection이 없는 경우 바로 생성됨
+        const meetupsCollection = db.collection("meetups");
+      
+        // 모든 문서 반환
+        const meetups = await meetupsCollection.find().toArray();
+      
+        // 데이터베이스 연결 차단
+        client.close();
+      
+        return {
+          props: {
+            meetups: meetups.map((meetup) => ({
+              title: meetup.title,
+              address: meetup.address,
+              image: meetup.image,
+              // 객체를 사용가능한 문자열로 변환
+              id: meetup._id.toString(),
+            })),
+          },
+          revalidate: 10,
+        };
+      }
+      
+      export default HomePage;
+      ```
--- NextJs/nextjs-course-v10-2/pages/index.js
+++ NextJs/nextjs-course-v10-2/pages/index.js
@@ -1,24 +1,6 @@
 import { useEffect, useState } from "react";
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Funguni_Island%2C_Pangani%2C_Tanga.jpg/1280px-Funguni_Island%2C_Pangani%2C_Tanga.jpg",
-    address: "Some address 5, 12345 Some City",
-    description: "This is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Funguni_Island%2C_Pangani%2C_Tanga.jpg/1280px-Funguni_Island%2C_Pangani%2C_Tanga.jpg",
-    address: "Some address 15, 12345 Some City",
-    description: "This is a second meetup!",
-  },
-];
 
 const HomePage = (props) => {
   // getStaticProps를 쓰면 상태를 관리할 필요가 없어짐
@@ -51,9 +33,34 @@ const HomePage = (props) => {
 // }
 
 export async function getStaticProps() {
+  // MongoClient 연결
+  const client = await MongoClient.connect(
+    "mongodb+srv://<username>:<password>@cluster0.uqu8pq0.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  // meetups에 연결 중인 데이터베이스 받아오기
+  // meetups 데이터베이스가 없는 경우 바로 생성됨
+  const db = client.db();
+
+  // meetups라는 이름의 collection에 접근
+  // meetups collection이 없는 경우 바로 생성됨
+  const meetupsCollection = db.collection("meetups");
+
+  // 모든 문서 반환
+  const meetups = await meetupsCollection.find().toArray();
+
+  // 데이터베이스 연결 차단
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        // 객체를 사용가능한 문자열로 변환
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 10,
   };
