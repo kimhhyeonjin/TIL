4084526659f8da4d7471cca5f91e636179520842
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-07-26 19:50:05 +0900

    0726 commit

--- NextJs/codingApple/fresh/app/list/page.js
+++ NextJs/codingApple/fresh/app/list/page.js
@@ -1,5 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
 export default function List() {
   const products = ["Tomatoes", "Pasta", "Coconut"];
+  const [cnt, setCnt] = useState([0, 0, 0]);
 
   return (
     <div>
@@ -9,6 +14,27 @@ export default function List() {
           <div className="food" key={idx}>
             <img src={`/food${idx}.png`} className="food-img" />
             <h4>{product}</h4>
+            {/* cnt 값이 0이면 - 버튼 비활성화 */}
+            <button
+              onClick={() => {
+                const temp = [...cnt];
+                temp[idx] -= 1;
+                setCnt(temp);
+              }}
+              disabled={cnt[idx] > 0 ? false : true}
+            >
+              -
+            </button>
+            <span> {cnt[idx]} </span>
+            <button
+              onClick={() => {
+                const temp = [...cnt];
+                temp[idx] += 1;
+                setCnt(temp);
+              }}
+            >
+              +
+            </button>
           </div>
         );
       })}
